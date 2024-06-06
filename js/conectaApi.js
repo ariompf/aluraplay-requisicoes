69ed3fdaa21eede48708b08c948ba614d2d9a004
Ariom Pimenta <ariompf@gmail.com>
2024-06-06 07:09:17 -0300

    add buscador

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -24,7 +24,14 @@ async function criaVideo(titulo, descricao, url, imagem) {
     return conexaoConvertida;
 };
 
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos/?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json();
+    return conexaoConvertida;
+} //essa função está me mostrando todos os vídeos em vez de um só referente a busca: revisar aulas
+
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
