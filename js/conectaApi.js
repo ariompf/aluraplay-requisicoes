44b82cef4a8efde83b2d2cf08618d5d8505cb994
Ariom Pimenta <ariompf@gmail.com>
2024-06-07 07:02:27 -0300

    aprimorando buscador

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -28,7 +28,7 @@ async function buscaVideo(termoDeBusca) {
     const conexao = await fetch(`http://localhost:3000/videos/?q=${termoDeBusca}`)
     const conexaoConvertida = conexao.json();
     return conexaoConvertida;
-} //essa função está me mostrando todos os vídeos em vez de um só referente a busca: revisar aulas
+} 
 
 export const conectaApi = {
     listaVideos,
